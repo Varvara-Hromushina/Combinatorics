27ed3b04050ad601a3da396abbdc8d205d5cf44f
Varvara-Hromushina <varvara.hromushina@yandex.ru>
2024-09-30 17:26:16 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,27 @@
+def PlacementWithRepetitions(elements, length):
+    if length == 0:
+        return [[]]
+    else:
+        result = []
+        for element in elements:
+            for tail in PlacementWithRepetitions(elements, length - 1):
+                result.append([element] + tail)
+        return result
+
+
+elements = []
+print("Введите количество элементов алфавита: ")
+n = int(input())
+print("Введите элементы алфавита: ")
+i = 0
+while (i != n):
+    elem = input()
+    elements.append(elem)
+    i += 1
+print("Введите значение k: ")
+length = int(input())
+
+print(f"Количество размещений с повторениями по {length} элементам = {len(PlacementWithRepetitions(elements, length))}")
+print("Размещения с повторениями: ")
+for element in PlacementWithRepetitions(elements, length):
+    print(element)
